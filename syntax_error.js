5f6c5cad276861686cecb0d13e42d7b610aa3a45
sagarGongit <sagarbhagwat1348.sb@gmail.com>
2025-07-23 16:29:53 +0530

    update

--- syntax_error.js
+++ syntax_error.js
@@ -25,3 +25,4 @@ checkout.addItem({ name: "Coffee Maker", price: "99.95" });
 checkout.addItem({ name: "Milk", price: 3.5 });
 
 console.log(checkout.getTotal()); // Expected issue resolved
+console.log(checkout.items); // total items
