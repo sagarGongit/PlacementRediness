03fe69143e8102fe53ac59c6a99317fd63e24b84
sagarGongit <sagarbhagwat1348.sb@gmail.com>
2025-07-24 15:49:23 +0530

    update

---
+++ data_privacy.js
@@ -0,0 +1,22 @@
+function createCounter() {
+  let count = 0;
+  function increment() {
+    count++;
+    return count;
+  }
+  function getCount() {
+    return count;
+  }
+  return {
+    increment,
+    getCount,
+  };
+}
+
+const counter = createCounter();
+
+console.log(counter.increment()); // Output: 1;
+
+console.log(counter.increment()); // Output: 2;
+
+console.log(counter.getCount()); // Output: 2;
